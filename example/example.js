23bc44c608d3c6f29de704efdc402507dbfbc0a4
nichoth <nichoth@gmail.com>
2015-05-23 00:48:58 -0700

    better api

--- example/example.js
+++ example/example.js
@@ -1,10 +1,23 @@
 var Pouch = require('pouchdb');
 var update = require('../');
+
+var db;
 require('little-pouch-db')( new Pouch('pouch-test') )
   .then(function(pouch) {
-    update(pouch).then(function(updater) {
-      setInterval(updater, 3000);
+    db = pouch;
+    return pouch.allDocs({
+      include_docs: true
     });
+  }).then(function(resp) {
+    var docs = resp.rows.map(function(row) {
+      return row.doc;
+    });
+    setInterval(function() {
+      // default updateFn returns promise from pouch.put()
+      update(db, docs).then(function(resp) {
+        console.log(resp);
+      });
+    }, 3000);
   })
 ;
 
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-module.exports = function updateRandomDoc(db, range, updateFn) {
+module.exports = function(db, docs, updateFn) {
 
-  range = range || {};
-
-  updateFn = updateFn || function(doc) {
+  updateFn = updateFn || function (doc) {
     if (!doc.updatedCount) {
       doc.updatedCount = 0;
     }
@@ -10,36 +8,12 @@ module.exports = function updateRandomDoc(db, range, updateFn) {
     return db.put(doc);
   };
 
-  var opts = {
-    include_docs: true,
-    limit: 100,
-    startkey: range.startkey,
-    endkey: range.endkey
-  };
-
-  function updater() {
-
+  if (!docs.length) {
+    return;
   }
+  var randomDoc = docs[Math.floor(Math.random() * docs.length)];
+  return db.get(randomDoc._id)
+    .then(updateFn)
+    .catch(console.log.bind(console));
 
-  return db.allDocs(opts)
-    .then(function(resp) {
-
-      var docs = resp.rows.map(function(row) {
-        return row.doc;
-      });
-
-      return function updater(docs) {
-        if (!docs.length) {
-          return;
-        }
-        var randomDoc = docs[Math.floor(Math.random() * docs.length)];
-        return db.get(randomDoc._id)
-          .then(updateFn)
-          .catch(console.log.bind(console))
-        ;
-      };
-
-    })
-    .catch(console.log.bind(console))
-  ;
 };
